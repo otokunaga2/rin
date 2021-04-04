7a245b2b65ad1bdd723324db10b875c914f4546a
Seiki Tokunaga <otokuna46@gmail.com>
2021-04-04 12:48:49 +0900

    Updates

--- main.go
+++ main.go
@@ -1,70 +1,68 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
-    "github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello world!\n")
+	fmt.Fprintf(w, "Hello world!\n")
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Currently system is running \n")
+	fmt.Fprintf(w, "Currently system is running \n")
 }
 
 func lineHandler(w http.ResponseWriter, r *http.Request) {
-    bot, err := linebot.New(
-						os.Getenv("CHANNEL_SECRET"),
-						os.Getenv("CHANNEL_TOKEN"),
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    events, err := bot.ParseRequest(r)
-    if err != nil {
-        if err == linebot.ErrInvalidSignature {
-            w.WriteHeader(400)
-        } else {
-            w.WriteHeader(500)
-        }
-        return
-    }
-    for _, event := range events {
-        if event.Type == linebot.EventTypeMessage {
-            switch message := event.Message.(type) {
-            case *linebot.TextMessage:
-                if message.Text == "こんにちは" {
-			id := message.ID
-			log.Print("Logging id :", id)
-			log.Print("Received From USER ID: ", event.Source.UserID)
-			replyMessage := fmt.Sprintf("はい、こんにちは")
-                	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-                	    log.Print(err)
-                	}
-                }
-            case *linebot.StickerMessage:
-                replyMessage := fmt.Sprintf(
-                    "sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-                if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-                    log.Print(err)
-                }
-            }
-        }
-    }
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				id := message.ID
+				log.Print("Logging id :", id)
+				log.Print("Received From USER ID: ", event.Source.UserID)
+				replyMessage := event.Message.Text
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			case *linebot.StickerMessage:
+				replyMessage := fmt.Sprintf(
+					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
 
 func main() {
-    port, _ := strconv.Atoi(os.Args[1])
-    fmt.Printf("Starting server at Port %d", port)
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/healthz", healthzHandler)
-    http.HandleFunc("/callback", lineHandler)
-    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	port, _ := strconv.Atoi(os.Args[1])
+	fmt.Printf("Starting server at Port %d", port)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/callback", lineHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
